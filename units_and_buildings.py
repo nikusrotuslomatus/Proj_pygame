65dd42ff2bee6cfa0c1b583d8bc74b34e5c1e7ea
nikusrotuslomatus <74095621+nikusrotuslomatus@users.noreply.github.com>
2020-11-11 15:11:06 +0300

    Add files via upload

---
+++ units_and_buildings.py
@@ -0,0 +1,120 @@
+from pygame_functions import *
+import random
+from collections import deque
+wood = "Desktop/project folder/wood.png"
+rock = "Desktop/project folder/rock.png"
+owl = "Desktop/project folder/owl.png"
+bush="Desktop/project folder/bush.png"
+warriorowl="Desktop/project folder/warriorowl.png"
+tree="Desktop/project folder/tree.png"
+warriortree="Desktop/project folder/warriortree.png"
+storage="Desktop/project folder/storage.png"
+def iscollectible(x):
+    if x.collectability==0:
+        return True
+    else:
+        return False
+class units(object):
+    class normal_unit(object):
+        def __init__(self,xpos,ypos):
+            self.xpos=xpos
+            self.ypos=ypos
+            self.speed=3
+            self.sprite=makeSprite(owl)
+            self.berry=0
+            self.sprite.hp=30
+            moveSprite(self.sprite,self.xpos,self.ypos)
+            showSprite(self.sprite)
+        def move(self):
+            if keyPressed("left"):
+                self.xpos -= self.speed
+            elif keyPressed("right"):
+                self.xpos += self.speed
+            if keyPressed("up"):
+                self.ypos -= self.speed
+            elif keyPressed("down"):
+                self.ypos += self.speed
+            moveSprite(self.sprite, self.xpos, self.ypos)
+        def harvest(self,bushes):
+            self.bushes=bushes
+            collectibles=allTouching(self.sprite)
+            for collectible in collectibles:
+                if collectible in self.bushes and iscollectible(collectible):
+                    changeSpriteImage(collectible,1)
+                    self.berry+=1
+                    collectible.collectability=1
+        def put_res(self,storage_name):
+            reslabel=makeLabel("ты положил "+str(self.berry)+" ягод на склад",50,30,30)
+            showLabel(reslabel)
+            self.storage_name=storage_name
+            self.storage_name.berry+=self.berry
+            self.berry=0
+            pause(1000)
+            hideLabel(reslabel)
+    class warrior(normal_unit):
+        def __init__(self,xpos,ypos):
+            self.xpos=xpos
+            self.ypos=ypos
+            self.speed=3
+            self.sprite=makeSprite(warriorowl)
+            self.sprite.hp=60
+            moveSprite(self.sprite,self.xpos,self.ypos)
+            showSprite(self.sprite)
+        def move(self):
+            if keyPressed("left"):
+                self.xpos -= self.speed
+            elif keyPressed("right"):
+                self.xpos += self.speed
+            if keyPressed("up"):
+                self.ypos -= self.speed
+            elif keyPressed("down"):
+                self.ypos += self.speed
+            moveSprite(self.sprite, self.xpos, self.ypos)
+        def attack(self):
+            enemies=allTouching(self.sprite)
+            for enemy in enemies:
+                enemy.hp-=10
+                if enemy.hp==0:
+                    hideSprite(enemy)
+class buildings(object):
+    class main_hall(object):
+        def __init__(self,builder):
+            self.xPos=builder.xpos
+            self.yPos=builder.ypos
+            self.building=None
+        def build(self):
+            self.building=makeSprite(tree)
+            self.building.hp=200
+            moveSprite(self.building,self.xPos,self.yPos)
+            showSprite(self.building)
+        def born_normal_unit(self):
+            self.normunit=units.normal_unit(self.xPos,self.yPos)
+            return self.normunit
+    class forge(object):
+        def __init__(self,builder):
+            self.xPos=builder.xpos
+            self.yPos=builder.ypos
+        def build(self):
+            self.building=makeSprite(warriortree)
+            self.building.hp=300
+            moveSprite(self.building,self.xPos,self.yPos)
+            showSprite(self.building)
+        def born_warrior(self):
+            self.warriorowl=units.warrior(self.xPos,self.yPos)
+            return self.warriorowl
+    class storage(object):
+        def __init__(self, builder):
+            self.xPos=builder.xpos
+            self.yPos=builder.ypos
+            self.building=None
+        def build(self):
+            self.building = makeSprite(storage)
+            self.building.hp = 200
+            self.building.berry = 0
+            self.building.wood = 0
+            self.building.stone = 0
+            self.building.x = self.xPos
+            self.building.y = self.yPos
+            moveSprite(self.building, self.xPos, self.yPos)
+            showSprite(self.building)
+            return self.building
